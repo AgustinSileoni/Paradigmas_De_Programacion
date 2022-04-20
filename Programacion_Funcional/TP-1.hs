43b7ccc5818f263cc8e59a55c1caecb39794e607
AgustinSileoni <agustinsileoni98@gmail.com>
2022-04-20 10:06:42 -0300

    Consigna Tp 1 funcional

---
+++ Programacion_Funcional/TP-1.hs
@@ -0,0 +1,9 @@
+data BTree a = Nil  | Leaf Int [a] | Node Int [a] [BTree a] deriving 
+
+-- creaar desde un [a]
+-- buscar el maximo
+-- buscar el minimo
+-- imprimir inoorder, preorder y postorder
+-- devolver un array de [a].
+-- mostrar la cantidad de item en el arbol.
+-- mostrar la profundida.
